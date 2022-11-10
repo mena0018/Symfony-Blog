21705e9f9c31db69966158bbd9c8035585cb47db
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-11-10 21:56:28 +0100

    Correction suggested by cs fixer

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController {
-  
-  #[Route('/', name: 'home')]
-  public function home(): Response {
-      return $this->render('pages/home.html.twig');
-  }
-  
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('pages/home.html.twig');
+    }
 }
